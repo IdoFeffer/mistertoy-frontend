be8ac5b30539ed0ca89a59514a72e83deaa89a65
Ido Feffer <idoppr1@gmail.com>
2025-05-05 15:33:36 +0300

    Added label multi-select in ToyFilter.jsx

--- src/cmps/ToyFilter.jsx
+++ src/cmps/ToyFilter.jsx
@@ -1,5 +1,27 @@
 import { useEffect, useRef, useState } from "react"
+import Select from "react-select"
 import { utilService } from "../services/util.service.js"
+
+const labelOptions = [
+  { value: "Updated", label: "Updated" },
+  { value: "Fun", label: "Fun" },
+  { value: "Sport", label: "Sport" },
+  { value: "Outdoor", label: "Outdoor" },
+  { value: "Mind Game", label: "Mind Game" },
+  { value: "Creative", label: "Creative" },
+  { value: "Building", label: "Building" },
+  { value: "Educational", label: "Educational" },
+  { value: "Puzzle", label: "Puzzle" },
+  { value: "On wheels", label: "On wheels" },
+  { value: "Indoor", label: "Indoor" },
+  { value: "Music", label: "Music" },
+  { value: "Box game", label: "Box game" },
+  { value: "Doll", label: "Doll" },
+  { value: "Fashion", label: "Fashion" },
+  { value: "Battery Powered", label: "Battery Powered" },
+  { value: "Adventure", label: "Adventure" },
+  { value: "Test", label: "Test" },
+]
 
 export function ToyFilter({ filterBy, onSetFilter }) {
   const [filterByToEdit, setFilterByToEdit] = useState({ ...filterBy })
@@ -13,7 +35,6 @@ export function ToyFilter({ filterBy, onSetFilter }) {
     let { value, name: field, type, selectedOptions } = target
 
     if (type === "number") value = +value
-
     if (type === "select-multiple") {
       value = Array.from(selectedOptions, (option) => option.value)
     }
@@ -23,20 +44,20 @@ export function ToyFilter({ filterBy, onSetFilter }) {
       else value = value === "true"
     }
 
-    if (field === 'labels' && value.includes('')) {
-      value = []
-    }
-    
-
     setFilterByToEdit((prevFilter) => ({ ...prevFilter, [field]: value }))
+  }
+
+  function handleLabelsChange(selectedOptions) {
+    const values = selectedOptions.map((opt) => opt.value)
+    setFilterByToEdit((prevFilter) => ({ ...prevFilter, labels: values }))
   }
 
   return (
     <section className="toy-filter full main-layout flex">
-
       <fieldset className="filter-form flex">
-      <legend>Toys Filter</legend>
-        {/* By name */}
+        <legend>Toys Filter</legend>
+
+        {/* Name */}
         <label htmlFor="name">Name:</label>
         <input
           type="text"
@@ -47,7 +68,7 @@ export function ToyFilter({ filterBy, onSetFilter }) {
           onChange={handleChange}
         />
 
-        {/* In stock */}
+        {/* In Stock */}
         <label htmlFor="inStock">In Stock:</label>
         <select
           id="inStock"
@@ -64,35 +85,18 @@ export function ToyFilter({ filterBy, onSetFilter }) {
           <option value="false">Out of stock</option>
         </select>
 
-        {/* Labels multiselect */}
-        <label htmlFor="labels">Label:</label>
-        <select
+        {/* Labels (using react-select) */}
+        <label htmlFor="labels">Labels:</label>
+        <Select
           id="labels"
           name="labels"
-          multiple
-          value={filterByToEdit.labels}
-          onChange={handleChange}
-        >
-          <option value="">All</option>
-          <option value="Updated">Updated</option>
-          <option value="Fun">Fun</option>
-          <option value="Sport">Sport</option>
-          <option value="Outdoor">Outdoor</option>
-          <option value="Mind Game">Mind Game</option>
-          <option value="Creative">Creative</option>
-          <option value="Building">Building</option>
-          <option value="Educational">Educational</option>
-          <option value="Puzzle">Puzzle</option>
-          <option value="On wheels">On wheels</option>
-          <option value="Indoor">Indoor</option>
-          <option value="Music">Music</option>
-          <option value="Box game">Box game</option>
-          <option value="Doll">Doll</option>
-          <option value="Fashion">Fashion</option>
-          <option value="Battery Powered">Battery Powered</option>
-          <option value="Adventure">Adventure</option>
-          <option value="Test">Test</option>
-        </select>
+          isMulti
+          options={labelOptions}
+          value={labelOptions.filter((opt) =>
+            filterByToEdit.labels.includes(opt.value)
+          )}
+          onChange={handleLabelsChange}
+        />
 
         {/* Sort By */}
         <label htmlFor="sortBy">Sort by:</label>
